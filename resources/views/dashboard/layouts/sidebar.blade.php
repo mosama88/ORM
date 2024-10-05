554b3f793df8e7d81ca5c53af6911d58d2189636
mosama88 <mosama88@hotmail>
2024-10-05 19:43:17 +0300

    Done 4

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar elevation-4" style="background-color: #ebedf1;">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src="{{ asset('assets') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
